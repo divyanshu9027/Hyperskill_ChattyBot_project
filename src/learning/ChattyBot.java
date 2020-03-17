6569bb7be2f72302f0544972a6c3b8ae4566b45b
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-03-17 13:51:08 +0530

    print the content as per the question.

--- src/learning/ChattyBot.java
+++ src/learning/ChattyBot.java
@@ -1,4 +1,12 @@
 package learning;
+import java.util.Scanner;
 
 public class ChattyBot {
+    final static Scanner scanner = new Scanner(System.in);
+    public static void greet() {
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("please, remind me your name.");
+    }
+
 }
