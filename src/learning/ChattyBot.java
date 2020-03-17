fc6378cc1446c450d261531d1e6095406b275db9
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-03-17 14:34:21 +0530

    code completed

--- src/learning/ChattyBot.java
+++ src/learning/ChattyBot.java
@@ -1,18 +1,28 @@
+/*
+* Created by : Intellij Idea
+* User : divyanshu9027
+* time : 2:20 P.M
+* date : 17/03/2020
+ */
 package learning;
 import java.util.Scanner;
 
 public class ChattyBot {
+    /*
+    * create the scanner object to fetch the data from the user.
+    * makes the scanner object static so it can not be changed.
+    */
     final static Scanner scanner = new Scanner(System.in);
-    public static void greet() {
+    public static void greet() {                            // create the greet method.
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2018.");
         System.out.println("please, remind me your name.");
     }
-    public static void name() {
+    public static void name() {                             // create the name method.
         String name = scanner.nextLine();
         System.out.println("What a great name you have," + name + "!");
     }
-    public static void age() {
+    public static void age() {                              // create the age method.
         System.out.println("Let me guess your age.");
         System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
         int rem3 = scanner.nextInt();
@@ -21,14 +31,14 @@ public class ChattyBot {
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
-    public static void count() {
+    public static void count() {                            // create the count method.
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int num = scanner.nextInt();
         for (int i = 0; i <= num; i++) {
             System.out.printf("%d!\n", i);
         }
     }
-    public static void com() {
+    public static void com() {                              // create the com method.
         System.out.println("Let's test your programming knowledge.");
         System.out.println("Why do we use methods?");
         System.out.println("1: To repeat a statement multiple times.");
@@ -41,11 +51,17 @@ public class ChattyBot {
             choice = scanner.nextInt();
         }
     }
-    public static void ans() {
+    public static void ans() {                      //create the ans method.
         System.out.println("Congratulations, have a nice day!");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        // call all the methods inside the main function.
+        greet();
+        name();
+        age();
+        count();
+        com();
+        ans();
 
     }
 
