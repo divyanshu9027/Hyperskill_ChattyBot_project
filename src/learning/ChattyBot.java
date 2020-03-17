331f56cb8b5affc0658797247f8bd486161cd455
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-03-17 13:58:34 +0530

    create the static method to count the number.

--- src/learning/ChattyBot.java
+++ src/learning/ChattyBot.java
@@ -20,7 +20,13 @@ public class ChattyBot {
         int rem7 = scanner.nextInt();
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
-
+    }
+    public static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
     }
 
+}
 }
